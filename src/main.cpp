735dffef044b37ce7f51f3a6296e3c2b36715c24
CatsaCode <catsacode@gmail.com>
2026-01-18 10:16:02 -0500

    Add ControllerFunctions as string maps
    Refactor ControllerCallbacks as strings instead of function pointers and call the functions with the same name.
    Using map<string, function<T>> automatically pairs each function with the name that should appear in each dropdown list. This should save the step of converting function pointers to strings or strings to function pointers, making it quicker to add functions in the future if ever need be.

--- src/main.cpp
+++ src/main.cpp
@@ -29,6 +29,7 @@
 #include "GlobalNamespace/NoteController.hpp"
 
 #include <cstdlib>
+#include <unordered_map>
 
 static modloader::ModInfo modInfo{MOD_ID, VERSION, 0};
 // Stores the ID and version of our mod, and is sent to
@@ -49,6 +50,7 @@ bool isModEnabled = true;
 bool fullPause = false;
 // Whether to show the three second countdown before resuming
 // bool resumeAnimation = false;
+float deadband = 0.1f;
 
 // References
 static UnityW<GlobalNamespace::AudioTimeSyncController> audioTimeSyncController = nullptr;
@@ -64,26 +66,33 @@ bool isPracticing = false;
 bool areReferencesSafe = false;
 bool isTimeSkipping = false;
 
+// TODO Move to mod config
 namespace ControllerCallbacks {
-    std::function<void()> buttonXOnClick = nullptr;
-    std::function<void()> buttonYOnClick = nullptr;
-    std::function<void()> leftThumbstickOnClick = nullptr;
-    std::function<void()> leftTriggerOnClick = nullptr;
-    std::function<void()> leftGripOnClick = nullptr;
-    std::function<void()> leftThumbstickHorizontalOnUpdate = nullptr;
-    std::function<void()> leftThumbstickVerticalOnUpdate = nullptr;
-    std::function<void()> leftTriggerOnUpdate = nullptr;
-    std::function<void()> leftGripOnUpdate = nullptr;
+    std::string buttonXOnClick = "";
+    std::string buttonYOnClick = "";
+    std::string leftThumbstickOnClick = "";
+    std::string leftTriggerOnClick = "";
+    std::string leftGripOnClick = "";
+    std::string buttonXOnHold = "";
+    std::string buttonYOnHold = "";
+    std::string leftThumbstickOnHold = "";
+    std::string leftTriggerOnHold = "";
+    std::string leftGripOnHold = "";
+    std::string leftThumbstickHorizontalOnUpdate = "";
+    std::string leftThumbstickVerticalOnUpdate = "";
 
-    std::function<void()> buttonAOnClick = nullptr;
-    std::function<void()> buttonBOnClick = nullptr;
-    std::function<void()> rightThumbstickOnClick = nullptr;
-    std::function<void()> rightTriggerOnClick = nullptr;
-    std::function<void()> rightGripOnClick = nullptr;
-    std::function<void()> rightThumbstickHorizontalOnUpdate = nullptr;
-    std::function<void()> rightThumbstickVerticalOnUpdate = nullptr;
-    std::function<void()> rightTriggerOnUpdate = nullptr;
-    std::function<void()> rightGripOnUpdate = nullptr;
+    std::string buttonAOnClick = "";
+    std::string buttonBOnClick = "";
+    std::string rightThumbstickOnClick = "";
+    std::string rightTriggerOnClick = "";
+    std::string rightGripOnClick = "";
+    std::string buttonAOnHold = "";
+    std::string buttonBOnHold = "";
+    std::string rightThumbstickOnHold = "";
+    std::string rightTriggerOnHold = "";
+    std::string rightGripOnHold = "";
+    std::string rightThumbstickHorizontalOnUpdate = "";
+    std::string rightThumbstickVerticalOnUpdate = "";
 }
 
 
@@ -195,6 +204,86 @@ void manualSetTimeScale(float timeScale) {
 
 
 // TODO Implement all the controller callback functions
+namespace ControllerFunctions {
+    template <typename T>
+    using FunctionList = std::unordered_map<std::string_view, std::function<T>>;
+
+    FunctionList<void()> clickFunctions = {
+        {"Skip Forward", [](){
+            PaperLogger.debug("(Click) Skip Forward");
+        }},
+        {"Skip Backward", [](){
+            PaperLogger.debug("(Click) Skip Backward");
+        }},
+        {"Increase Speed", [](){
+            PaperLogger.debug("(Click) Increase Speed");
+        }},
+        {"Decrease Speed", [](){
+            PaperLogger.debug("(Click) Decrease Speed");
+        }},
+        {"Reset Speed", [](){
+            PaperLogger.debug("(Click) Reset Speed");
+        }},
+        {"Pause / Play", [](){
+            PaperLogger.debug("(Click) Pause / Play");
+        }},
+        {"Add Marker", [](){
+            PaperLogger.debug("(Click) Add Marker");
+        }},
+        {"Remove Marker", [](){
+            PaperLogger.debug("(Click) Remove Marker");
+        }},
+        {"Next Marker", [](){
+            PaperLogger.debug("(Click) Next Marker");
+        }},
+        {"Previous Marker", [](){
+            PaperLogger.debug("(Click) Previous Marker");
+        }}
+    };
+
+    FunctionList<void(float)> holdFunctions = {
+        {"Skip Forward", [](float input){
+            if(input < deadband) return;
+            PaperLogger.debug("(Hold) Skip Forward: {}", input);
+        }},
+        {"Skip Backward", [](float input){
+            if(input < deadband) return;
+            PaperLogger.debug("(Hold) Skip Backward: {}", input);
+        }},
+        {"Increase Speed", [](float input){
+            if(input < deadband) return;
+            PaperLogger.debug("(Hold) Increase Speed: {}", input);
+        }},
+        {"Decrease Speed", [](float input){
+            if(input < deadband) return;
+            PaperLogger.debug("(Hold) Decrease Speed: {}", input);
+        }}
+    };
+
+    FunctionList<void(float)> thumbstickFunctions = {
+        {"Skip", [](float input){
+            if(abs(input) < deadband) return;
+            PaperLogger.debug("(Thumbstick) Skip: {}", input);
+        }},
+        {"Speed", [](float input){
+            if(abs(input) < deadband) return;
+            PaperLogger.debug("(Thumbstick) Speed: {}", input);
+        }}
+    };
+
+    void invoke(FunctionList<void()>& list, std::string_view name) {
+        if(name.empty()) return;
+        if(!list.contains(name)) {PaperLogger.error("Could not invoke function: '{}'", name); return;}
+        list[name]();
+    }
+
+    template <typename T>
+    void invoke(FunctionList<void(T)>& list, std::string_view name, T input){
+        if(name.empty()) return;
+        if(!list.contains(name)) {PaperLogger.error("Could not invoke function: '{}'", name); return;}
+        list[name](input);
+    }
+}
 
 
 struct ControllerState {
@@ -219,25 +308,25 @@ struct ControllerState {
     float rightGripInput = 0;
 
     void update() {
-        isButtonXHeld         = MetaCore::Input::GetPressed(MetaCore::Input::Controllers::Left, MetaCore::Input::Buttons::AX);
-        isButtonYHeld         = MetaCore::Input::GetPressed(MetaCore::Input::Controllers::Left, MetaCore::Input::Buttons::BY);
-        isLeftThumbstickHeld  = MetaCore::Input::GetPressed(MetaCore::Input::Controllers::Left, MetaCore::Input::Buttons::Thumbstick);
-        isLeftTriggerHeld     = MetaCore::Input::GetPressed(MetaCore::Input::Controllers::Left, MetaCore::Input::Buttons::Trigger);
-        isLeftGripHeld        = MetaCore::Input::GetPressed(MetaCore::Input::Controllers::Left, MetaCore::Input::Buttons::Grip);
+        isButtonXHeld         = MetaCore::Input::GetPressed(MetaCore::Input::Controllers::Left,  MetaCore::Input::Buttons::AX);
+        isButtonYHeld         = MetaCore::Input::GetPressed(MetaCore::Input::Controllers::Left,  MetaCore::Input::Buttons::BY);
+        isLeftThumbstickHeld  = MetaCore::Input::GetPressed(MetaCore::Input::Controllers::Left,  MetaCore::Input::Buttons::Thumbstick);
+        isLeftTriggerHeld     = MetaCore::Input::GetPressed(MetaCore::Input::Controllers::Left,  MetaCore::Input::Buttons::Trigger);
+        isLeftGripHeld        = MetaCore::Input::GetPressed(MetaCore::Input::Controllers::Left,  MetaCore::Input::Buttons::Grip);
         isButtonAHeld         = MetaCore::Input::GetPressed(MetaCore::Input::Controllers::Right, MetaCore::Input::Buttons::AX);
         isButtonBHeld         = MetaCore::Input::GetPressed(MetaCore::Input::Controllers::Right, MetaCore::Input::Buttons::BY);
         isRightThumbstickHeld = MetaCore::Input::GetPressed(MetaCore::Input::Controllers::Right, MetaCore::Input::Buttons::Thumbstick);
         isRightTriggerHeld    = MetaCore::Input::GetPressed(MetaCore::Input::Controllers::Right, MetaCore::Input::Buttons::Trigger);
         isRightGripHeld       = MetaCore::Input::GetPressed(MetaCore::Input::Controllers::Right, MetaCore::Input::Buttons::Grip);
 
-        leftThumbstickHorizontalInput  = MetaCore::Input::GetAxis(MetaCore::Input::Controllers::Left, MetaCore::Input::Axes::ThumbstickHorizontal);
-        leftThumbstickVerticalInput    = MetaCore::Input::GetAxis(MetaCore::Input::Controllers::Left, MetaCore::Input::Axes::ThumbstickVertical);
-        leftTriggerInput               = MetaCore::Input::GetAxis(MetaCore::Input::Controllers::Left, MetaCore::Input::Axes::TriggerStrength);
-        leftGripInput                  = MetaCore::Input::GetAxis(MetaCore::Input::Controllers::Left, MetaCore::Input::Axes::GripStrength);
-        rightThumbstickHorizontalInput = MetaCore::Input::GetAxis(MetaCore::Input::Controllers::Left, MetaCore::Input::Axes::ThumbstickHorizontal);
-        rightThumbstickVerticalInput   = MetaCore::Input::GetAxis(MetaCore::Input::Controllers::Left, MetaCore::Input::Axes::ThumbstickVertical);
-        rightTriggerInput              = MetaCore::Input::GetAxis(MetaCore::Input::Controllers::Left, MetaCore::Input::Axes::TriggerStrength);
-        rightGripInput                 = MetaCore::Input::GetAxis(MetaCore::Input::Controllers::Left, MetaCore::Input::Axes::GripStrength);
+        leftThumbstickHorizontalInput  = MetaCore::Input::GetAxis(MetaCore::Input::Controllers::Left,  MetaCore::Input::Axes::ThumbstickHorizontal);
+        leftThumbstickVerticalInput    = MetaCore::Input::GetAxis(MetaCore::Input::Controllers::Left,  MetaCore::Input::Axes::ThumbstickVertical);
+        leftTriggerInput               = MetaCore::Input::GetAxis(MetaCore::Input::Controllers::Left,  MetaCore::Input::Axes::TriggerStrength);
+        leftGripInput                  = MetaCore::Input::GetAxis(MetaCore::Input::Controllers::Left,  MetaCore::Input::Axes::GripStrength);
+        rightThumbstickHorizontalInput = MetaCore::Input::GetAxis(MetaCore::Input::Controllers::Right, MetaCore::Input::Axes::ThumbstickHorizontal);
+        rightThumbstickVerticalInput   = MetaCore::Input::GetAxis(MetaCore::Input::Controllers::Right, MetaCore::Input::Axes::ThumbstickVertical);
+        rightTriggerInput              = MetaCore::Input::GetAxis(MetaCore::Input::Controllers::Right, MetaCore::Input::Axes::TriggerStrength);
+        rightGripInput                 = MetaCore::Input::GetAxis(MetaCore::Input::Controllers::Right, MetaCore::Input::Axes::GripStrength);
     }
 };
 ControllerState controllerState;
@@ -245,25 +334,33 @@ void handleControllerInputs() {
     ControllerState oldControllerState = controllerState;
     controllerState.update();
 
-    if(!oldControllerState.isButtonXHeld         && controllerState.isButtonXHeld         && ControllerCallbacks::buttonXOnClick)         ControllerCallbacks::buttonXOnClick();
-    if(!oldControllerState.isButtonYHeld         && controllerState.isButtonYHeld         && ControllerCallbacks::buttonYOnClick)         ControllerCallbacks::buttonYOnClick();
-    if(!oldControllerState.isLeftThumbstickHeld  && controllerState.isLeftThumbstickHeld  && ControllerCallbacks::leftThumbstickOnClick)  ControllerCallbacks::leftThumbstickOnClick();
-    if(!oldControllerState.isLeftTriggerHeld     && controllerState.isLeftTriggerHeld     && ControllerCallbacks::leftTriggerOnClick)     ControllerCallbacks::leftTriggerOnClick();
-    if(!oldControllerState.isLeftGripHeld        && controllerState.isLeftGripHeld        && ControllerCallbacks::leftGripOnClick)        ControllerCallbacks::leftGripOnClick();
-    if(!oldControllerState.isButtonAHeld         && controllerState.isButtonAHeld         && ControllerCallbacks::buttonAOnClick)         ControllerCallbacks::buttonAOnClick();
-    if(!oldControllerState.isButtonBHeld         && controllerState.isButtonBHeld         && ControllerCallbacks::buttonBOnClick)         ControllerCallbacks::buttonBOnClick();
-    if(!oldControllerState.isRightThumbstickHeld && controllerState.isRightThumbstickHeld && ControllerCallbacks::rightThumbstickOnClick) ControllerCallbacks::rightThumbstickOnClick();
-    if(!oldControllerState.isRightTriggerHeld    && controllerState.isRightTriggerHeld    && ControllerCallbacks::rightTriggerOnClick)    ControllerCallbacks::rightTriggerOnClick();
-    if(!oldControllerState.isRightGripHeld       && controllerState.isRightGripHeld       && ControllerCallbacks::rightGripOnClick)       ControllerCallbacks::rightGripOnClick();
+    if(!oldControllerState.isButtonXHeld         && controllerState.isButtonXHeld)         ControllerFunctions::invoke(ControllerFunctions::clickFunctions, ControllerCallbacks::buttonXOnClick);
+    if(!oldControllerState.isButtonYHeld         && controllerState.isButtonYHeld)         ControllerFunctions::invoke(ControllerFunctions::clickFunctions, ControllerCallbacks::buttonYOnClick);
+    if(!oldControllerState.isLeftThumbstickHeld  && controllerState.isLeftThumbstickHeld)  ControllerFunctions::invoke(ControllerFunctions::clickFunctions, ControllerCallbacks::leftThumbstickOnClick);
+    if(!oldControllerState.isLeftTriggerHeld     && controllerState.isLeftTriggerHeld)     ControllerFunctions::invoke(ControllerFunctions::clickFunctions, ControllerCallbacks::leftTriggerOnClick);
+    if(!oldControllerState.isLeftGripHeld        && controllerState.isLeftGripHeld)        ControllerFunctions::invoke(ControllerFunctions::clickFunctions, ControllerCallbacks::leftGripOnClick);
+    if(!oldControllerState.isButtonAHeld         && controllerState.isButtonAHeld)         ControllerFunctions::invoke(ControllerFunctions::clickFunctions, ControllerCallbacks::buttonAOnClick);
+    if(!oldControllerState.isButtonBHeld         && controllerState.isButtonBHeld)         ControllerFunctions::invoke(ControllerFunctions::clickFunctions, ControllerCallbacks::buttonBOnClick);
+    if(!oldControllerState.isRightThumbstickHeld && controllerState.isRightThumbstickHeld) ControllerFunctions::invoke(ControllerFunctions::clickFunctions, ControllerCallbacks::rightThumbstickOnClick);
+    if(!oldControllerState.isRightTriggerHeld    && controllerState.isRightTriggerHeld)    ControllerFunctions::invoke(ControllerFunctions::clickFunctions, ControllerCallbacks::rightTriggerOnClick);
+    if(!oldControllerState.isRightGripHeld       && controllerState.isRightGripHeld)       ControllerFunctions::invoke(ControllerFunctions::clickFunctions, ControllerCallbacks::rightGripOnClick);
 
-    if(ControllerCallbacks::leftThumbstickHorizontalOnUpdate)  ControllerCallbacks::leftThumbstickHorizontalOnUpdate();
-    if(ControllerCallbacks::leftThumbstickVerticalOnUpdate)    ControllerCallbacks::leftThumbstickVerticalOnUpdate();
-    if(ControllerCallbacks::leftTriggerOnUpdate)               ControllerCallbacks::leftTriggerOnUpdate();
-    if(ControllerCallbacks::leftGripOnUpdate)                  ControllerCallbacks::leftGripOnUpdate();
-    if(ControllerCallbacks::rightThumbstickHorizontalOnUpdate) ControllerCallbacks::rightThumbstickHorizontalOnUpdate();
-    if(ControllerCallbacks::rightThumbstickVerticalOnUpdate)   ControllerCallbacks::rightThumbstickVerticalOnUpdate();
-    if(ControllerCallbacks::rightTriggerOnUpdate)              ControllerCallbacks::rightTriggerOnUpdate();
-    if(ControllerCallbacks::rightGripOnUpdate)                 ControllerCallbacks::rightGripOnUpdate();
+    ControllerFunctions::invoke<float>(ControllerFunctions::holdFunctions, ControllerCallbacks::buttonXOnHold,         controllerState.isButtonXHeld);
+    ControllerFunctions::invoke<float>(ControllerFunctions::holdFunctions, ControllerCallbacks::buttonYOnHold,         controllerState.isButtonYHeld);
+    ControllerFunctions::invoke<float>(ControllerFunctions::holdFunctions, ControllerCallbacks::leftThumbstickOnHold,  controllerState.isLeftThumbstickHeld);
+    ControllerFunctions::invoke<float>(ControllerFunctions::holdFunctions, ControllerCallbacks::buttonAOnHold,         controllerState.isButtonAHeld);
+    ControllerFunctions::invoke<float>(ControllerFunctions::holdFunctions, ControllerCallbacks::buttonBOnHold,         controllerState.isButtonBHeld);
+    ControllerFunctions::invoke<float>(ControllerFunctions::holdFunctions, ControllerCallbacks::rightThumbstickOnHold, controllerState.isRightThumbstickHeld);
+
+    ControllerFunctions::invoke<float>(ControllerFunctions::holdFunctions, ControllerCallbacks::leftTriggerOnHold,   controllerState.leftTriggerInput);
+    ControllerFunctions::invoke<float>(ControllerFunctions::holdFunctions, ControllerCallbacks::leftGripOnHold,      controllerState.leftGripInput);
+    ControllerFunctions::invoke<float>(ControllerFunctions::holdFunctions, ControllerCallbacks::rightTriggerOnHold,  controllerState.rightTriggerInput);
+    ControllerFunctions::invoke<float>(ControllerFunctions::holdFunctions, ControllerCallbacks::rightGripOnHold,     controllerState.rightGripInput);
+
+    ControllerFunctions::invoke<float>(ControllerFunctions::thumbstickFunctions, ControllerCallbacks::leftThumbstickHorizontalOnUpdate,  controllerState.leftThumbstickHorizontalInput);
+    ControllerFunctions::invoke<float>(ControllerFunctions::thumbstickFunctions, ControllerCallbacks::leftThumbstickVerticalOnUpdate,    controllerState.leftThumbstickVerticalInput);
+    ControllerFunctions::invoke<float>(ControllerFunctions::thumbstickFunctions, ControllerCallbacks::rightThumbstickHorizontalOnUpdate, controllerState.rightThumbstickHorizontalInput);
+    ControllerFunctions::invoke<float>(ControllerFunctions::thumbstickFunctions, ControllerCallbacks::rightThumbstickVerticalOnUpdate,   controllerState.rightThumbstickVerticalInput);
 }
 
 void handleMapStart() {
